8a9fb16ae02ab077950989216ea87e2803b145e1
Hi-techhitesh <hiteshlande2294@gmail.com>
2026-01-20 04:04:47 +0530

    allowed host changed

--- gram_panchayat/settings.py
+++ gram_panchayat/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-dev-key-change-in-production')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,gram-panchayat-fubgao.onrender.com', cast=Csv())
 
 # Application definition
 INSTALLED_APPS = [
