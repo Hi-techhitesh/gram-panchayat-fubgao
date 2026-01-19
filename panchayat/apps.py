2c09fe2cfb897de12ccc0f71e2a0fa8db38db079
Hi-techhitesh <hiteshlande2294@gmail.com>
2026-01-20 03:38:23 +0530

    updated

--- panchayat/apps.py
+++ panchayat/apps.py
@@ -5,3 +5,17 @@ class PanchayatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'panchayat'
     verbose_name = 'Gram Panchayat Management'
+
+
+    # At the bottom of settings.py (or in a separate startup script)
+import os
+from django.contrib.auth import get_user_model
+
+if os.getenv("DJANGO_SUPERUSER_USERNAME") and os.getenv("DJANGO_SUPERUSER_PASSWORD"):
+    User = get_user_model()
+    if not User.objects.filter(username=os.getenv("DJANGO_SUPERUSER_USERNAME")).exists():
+        User.objects.create_superuser(
+            username=os.getenv("DJANGO_SUPERUSER_USERNAME"),
+            email=os.getenv("DJANGO_SUPERUSER_EMAIL", ""),
+            password=os.getenv("DJANGO_SUPERUSER_PASSWORD"),
+        )
